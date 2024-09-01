f211c21519df1c3ee711251524bbd66d7764f062
MakWigglz <wigglz734@gmail.com>
2024-09-02 00:04:33 +0300

    feat: Add main method to AndNow class
    This commit adds a main method to the AndNow class in order to print a message to the console. The message includes a philosophical question and a statement about the study of the physical world.

--- .vscode/demo/src/main/java/com/webapp/AndNow.java
+++ .vscode/demo/src/main/java/com/webapp/AndNow.java
@@ -1,4 +1,8 @@
 package com.webapp;
 public class AndNow {
-    
-}
+    public static void main(String[]args) {
+        System.out.println("Atoms or Consciousness?");
+        System.out.print("The 7 Liberal Arts & Sciences is a symbol of our duty as Initiates to study the physical world");    
+        }      
+    }
+
