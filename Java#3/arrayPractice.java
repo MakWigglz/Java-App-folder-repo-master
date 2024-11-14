f056ce043baec11aca671fb96b024fb91929e22b
MakWigglz <wigglz734@gmail.com>
2024-11-14 03:13:20 +0200

    Create arrayPractice.java

---
+++ Java#3/arrayPractice.java
@@ -0,0 +1,32 @@
+//create an array of type integer and initialize it, in one line.
+int[] age = {10, 15, 20, 25, 30}; 
+
+int[] age = new int[5];
+//initialize the 5 indexes from index 0 to index 4.
+age[0]= [12];
+age[1]= [17];
+age[2]=[20];
+age[3]=[22];
+age[4]=[25];
+
+//access elements in an array through the index numer, i.e. the number of elements in the array.
+array[index];
+
+class Main {
+  public static void(String[] args) {
+    int[] age = {12, 4, 5, 2, 5};
+    System.out.println("Accessing Elements of Array:");
+    System.out.println("First Element:" + age[0]);
+    System.out.println("Second Element") + age[1];
+    System.out.println("3rd Element") + age[2];
+    System.out.println("4th Element") + age[3];
+    System.out.println("5th Element") + age[4];
+  }
+}
+// Output:
+/* Accessing Elements of Array:
+First Element: 12
+Second Element: 4
+Third Element: 5
+Fourth Element: 2
+Fifth Element: 5 */
