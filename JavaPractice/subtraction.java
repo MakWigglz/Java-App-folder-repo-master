4119a6213c52ff434db3c74ef80ea695bf8f1b94
MakWigglz <wigglz734@gmail.com>
2024-11-23 03:34:56 +0200

    Create subtraction.java

---
+++ JavaPractice/subtraction.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class Subtraction{
+  public static void main(String[] args){
+    //init.
+    int num1=30, num2=15;
+    int sub=nm1-num2;
+
+    System.out.println("Sum is: " + sum)
+  }
+    // then we compile the java code using Terminal, like this from my local machine.
+  /*
+  (base) amakki@As-MacBook-Pro ~ % cd /Users/amakki/Documents/Coding-Design/GitHub/Java-App-Folder-Repo-Master/JavaPractice
+(base) amakki@As-MacBook-Pro JavaPractice % javac javaPractice.java
+(base) amakki@As-MacBook-Pro JavaPractice % java addition
+The firsr number is: 23
+The sum of the two numbers is: 56
+(base) amakki@As-MacBook-Pro JavaPractice % 
+  */
+}
