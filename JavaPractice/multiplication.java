c2cf1a0cb0dd122ab97d9bc95074435a08865e95
A Makki <wigglz734@gmail.com>
2024-12-27 01:26:23 +0200

    practice

---
+++ JavaPractice/multiplication.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class multiplication{
+    public static void main(String[] args){
+        // Innitializing values
+        int num1 = 10, num2 = 30;
+        // Performing multiplication
+        System.out.println("num1 * num2 = " + (num1*num2));
+
+
+
+    }
+}
