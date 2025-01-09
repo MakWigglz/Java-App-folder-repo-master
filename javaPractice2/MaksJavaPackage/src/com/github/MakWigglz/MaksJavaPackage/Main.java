00d86b6e9f452e0e89397f3332f1bc07363999c2
A Makki <wigglz734@gmail.com>
2025-01-09 23:11:59 +0200

    MadLIbPackageJava

--- javaPractice2/MaksJavaPackage/src/com/github/MakWigglz/MaksJavaPackage/Main.java
+++ javaPractice2/MaksJavaPackage/src/com/github/MakWigglz/MaksJavaPackage/Main.java
@@ -1,11 +1,14 @@
-package com.github.MakWigglz.MaksJavaPackage;
+package com.github.MakWigglz.maksjavapackage;
 
 public class Main {
     public static void main(String[] args) {
         // Your main method implementation
         String name = "Ahmad Makki";
         int age = 50;
-        boolean IsCoder = true;
-
+        boolean isCoder = true;
+        
+        System.out.println("Name:  " + name);
+        System.out.println("Age: " + age);
+        System.out.println("He is a self taught Coder: " + isCoder);
     }
-}
+} 
